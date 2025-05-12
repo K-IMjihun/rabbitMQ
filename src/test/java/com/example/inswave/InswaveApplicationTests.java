6e56f484b55c25b8b57ac3a47d987a6d2bd6cfcf
K-IMjihun <kjh950901@gmail.com>
2025-05-12 10:38:49 +0900

    test : jenkins test 4

--- src/test/java/com/example/inswave/InswaveApplicationTests.java
+++ src/test/java/com/example/inswave/InswaveApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InswaveApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
